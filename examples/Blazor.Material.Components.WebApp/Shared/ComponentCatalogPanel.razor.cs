e3796c3d7f90427936e69bfe54b9757427372e1d
Leonardo D'Alonzo <ldalonzo@gmail.com>
2020-01-11 21:32:23 +0000

    refactor(demo): Factor out boilerplate code into a reusable component

---
+++ examples/Blazor.Material.Components.WebApp/Shared/ComponentCatalogPanel.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Material.Components.WebApp.Shared
+{
+    public partial class ComponentCatalogPanel : ComponentBase
+    {
+        [Parameter] public string Title { get; set; }
+
+        [Parameter] public RenderFragment HeroComponent { get; set; }
+
+        [Parameter] public RenderFragment HeroComponentOptions { get; set; }
+
+        [Parameter] public RenderFragment Demos { get; set; }
+    }
+}
