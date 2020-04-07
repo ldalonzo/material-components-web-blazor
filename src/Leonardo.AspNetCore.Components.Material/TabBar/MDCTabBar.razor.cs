21c65fdeb510516f866cb7e416b527a1acd9ca38
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2020-04-07 22:05:26 +0100

    feat(tab-bar): Initial scaffolding

---
+++ src/Leonardo.AspNetCore.Components.Material/TabBar/MDCTabBar.razor.cs
@@ -0,0 +1,11 @@
+namespace Leonardo.AspNetCore.Components.Material.TabBar
+{
+    /// <summary>
+    /// Tabs organize and allow navigation between groups of content that are related and at the same level of hierarchy.
+    /// The Tab Bar contains the Tab Scroller and Tab components.
+    /// </summary>
+    public partial class MDCTabBar
+    {
+
+    }
+}
