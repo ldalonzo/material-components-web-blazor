b80d4dc359be644781c8ea9416ab3992c45eaeaa
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2021-05-09 12:28:15 +0100

    refactor: No point in self-hosting icons

--- examples/Blazor.Material.Components.WebApp/app.js
+++ examples/Blazor.Material.Components.WebApp/app.js
@@ -3,7 +3,7 @@ import './app.scss'
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 const appInsights = new ApplicationInsights({ config: {
-  instrumentationKey: '2430ac1c-7d16-480a-9a1d-08071e3da25a'
+  instrumentationKey: '2656cbd4-20cb-404e-8a26-af24c943c357'
 } })
 
 appInsights.loadAppInsights()
